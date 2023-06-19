07319cd737a80a8bb19d69959be187a5f86163d2
thismars <yhklip990@gmail.com>
2023-06-19 16:57:40 +0900

    update: Fixed error

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const port = app.get(ConfigService).get('PORT') || 3000;
+  const port = app.get(ConfigService).get('APP_PORT') || 3000;
   
   app.enableCors();
 
